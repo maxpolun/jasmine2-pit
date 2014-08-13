41f2b7ea57134781f0e5684e549c8af0cd4ac0f1
Max Polun <mpolun@mdsol.com>
2014-08-13 14:04:01 -0400

    fix dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,10 +70,10 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-node-inspector')
   grunt.loadNpmTasks('grunt-concurrent')
-  
+
   grunt.registerMultiTask('spec', 'Run node jasmine specs', function(){
     var done = this.async();
-    var jasmineLib = require('minijasminenode');
+    var jasmineLib = require('minijasminenode2');
     this.data.specs = grunt.file.expand(this.data.specs)
     this.data.onComplete = done
     jasmineLib.executeSpecs(this.data)
